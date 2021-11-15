a509785109412672a4390de8d8f8f5262868ae52
Prasad27s <94112373+Prasad27s@users.noreply.github.com>
2021-11-15 16:51:20 +0000

    Shop entry-exit code (MovePlayer) added to the main scene

--- Assets/ShopExit.cs
+++ Assets/ShopExit.cs
@@ -4,15 +4,33 @@ using UnityEngine;
 
 public class ShopExit : MonoBehaviour
 {
+    public GameObject Shop;
+    public GameObject player;
+    //public GameObject EnterShop;
+    public GameObject ExitShop;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+     //EnterShop.SetActive(false);
+        ExitShop.SetActive(false);   
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        //EnterShop.SetActive(true);
+        ExitShop.SetActive(true);
+    //player.transform.position = Shop.transform.position;
+    }
+void OnTriggerExit2D(Collider2D other)
+    {
+        //EnterShop.SetActive(false);
+        ExitShop.SetActive(false);
+
     }
 }
