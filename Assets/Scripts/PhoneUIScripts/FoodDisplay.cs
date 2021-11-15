da5111f19dc4b115e005fa406ef84023a71c78f7
cfdj <callumderry@gmail.com>
2021-11-15 12:50:07 +0000

    Walk for crash course, phone UI prefab

---
+++ Assets/Scripts/PhoneUIScripts/FoodDisplay.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class FoodDisplay : MonoBehaviour
+{
+    //scriptable object, whichever shop this is
+    public Image sprite;
+    public Text shopName;
+    public Text price;
+    public Text foodType;
+    void Awake()
+    {
+        
+    }
+
+    
+}
