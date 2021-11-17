8b085e1d8650983ff79584460bb39c3327c2b4b8
Prasad27s <94112373+Prasad27s@users.noreply.github.com>
2021-11-17 13:06:37 +0000

    FoodInTheShop script updated for disabling food when player doesn't have enough money

--- Assets/Scenes/Prasads Sandbox/FoodInTheShop.cs
+++ Assets/Scenes/Prasads Sandbox/FoodInTheShop.cs
@@ -8,17 +8,49 @@ public class FoodInTheShop : MonoBehaviour
     public Image sprite;
     public Text price;
     private Food food;
+
+   public static List<FoodInTheShop> fooditems = new List<FoodInTheShop>();
+
     void Awake()
     {
-
+        fooditems.Add(this);
     }
-    public void SetFood(Food newFood){
+
+//defining food in the shop
+    public void SetFood(Food newFood)
+    {
         food = newFood;
         sprite.sprite = food.image;
         price.text = "$" + food.price;
+        disableFood();
     }
-    public void tryToBuy(){
+
+// clicking on food to buy it i.e. reduce the money
+    public void tryToBuy()
+    {
+        
         GameManager.gameManager.BuyFood(food);
 
+// disable food after a click on food if it becomes unaffordable 
+        for (int i=0; i<fooditems.Count; i++)
+        {
+                fooditems[i].disableFood();
+
+        }
     }
+
+  //disabling food when the player cannot afford it
+    public void disableFood()
+{
+    
+    if (!GameManager.gameManager.CheckMoney(food.price))
+    { 
+    gameObject.GetComponent<Button>().interactable = false;
+    }
+    else
+    {
+        gameObject.GetComponent<Button>().interactable = true;
+    }
+    
+}
 }
